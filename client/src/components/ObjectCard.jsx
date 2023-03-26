6c702e64f8a38ca6dc9d7723775f81b39ba48407
Aaron Amodt <aaron@amodttech.com>
2023-03-25 21:39:52 -0400

    changed object card layout/styling

--- client/src/components/ObjectCard.jsx
+++ client/src/components/ObjectCard.jsx
@@ -1,46 +1,25 @@
 import React from "react";
 
-function ObjectCard({artObject, key}) {
+function ObjectCard({ artObject, element }) {
   return (
-    <li key={key} className="object-card">
-      <p>
-        objectID:{" "}
-        <a href={artObject.objectURL} target="_blank">
-          {artObject.objectID}
-        </a>
-      </p>
-
-      <p>title: {artObject.title}</p>
-
-      {artObject.artistDisplayName ? (
-        <p>artistDisplayName: {artObject.artistDisplayName}</p>
-      ) : (
-        <p>artistDisplayName: Unknown</p>
-      )}
-
-      <p>artistdisplayBio: {artObject.artistDisplayBio}</p>
-
-      <p>artistNationality: {artObject.artistNationality}</p>
-
-      <p>objectDate: {artObject.objectDate}</p>
-
-      <p>dimensions: {artObject.dimensions}</p>
-
-      <p>medium: {artObject.medium}</p>
-
-      <p>department: {artObject.department}</p>
-
-      {artObject.artistDisplayName ? (
-        <p>artistDisplayName: {artObject.artistDisplayName}</p>
-      ) : null}
-
+    <li key={element} className="object-card">
       {artObject.primaryImageSmall ? (
         <a href={artObject.primaryImage} target="_blank">
           <img src={artObject.primaryImageSmall} alt={artObject.title} />
         </a>
       ) : (
-        "no image available"
+        <div className="no-image">no image</div>
       )}
+
+      <h4 className="object-title">{artObject.title}</h4>
+      <h5 className="object-artist">{artObject.artistDisplayName}</h5>
+      <h5 className="object-date">{artObject.objectDate}</h5>
+
+      <div className="object-id-container">ID: 
+        <a href={artObject.objectURL} target="_blank">
+          {artObject.objectID}
+        </a>
+      </div>
     </li>
   );
 }
